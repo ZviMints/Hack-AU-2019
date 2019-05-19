4e8bcdcf60852f375c6194b863a7c8421a99efc3
ZviMints <ZviMints@gmail.com>
2019-05-19 23:06:38 +0300

    added dynamic

--- application.py
+++ application.py
@@ -1,14 +1,5 @@
-from flask import Flask, render_template, session, redirect, url_for, request
-from flask_session import Session
-from tempfile import mkdtemp
-import math 
-
+from flask import Flask, request, render_template
 app = Flask(__name__)
-
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 
 @app.route("/")
 def index():
