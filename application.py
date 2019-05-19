c216d892e642bf2d31f1692ef90a37fc99c6fb08
ZviMints <ZviMints@gmail.com>
2019-05-19 17:19:39 +0300

    fix proc

--- application.py
+++ application.py
@@ -1,5 +1,15 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, session, redirect, url_for
+from flask_session import Session
+from tempfile import mkdtemp
+import math 
+
+
 app = Flask(__name__)
+
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index():
